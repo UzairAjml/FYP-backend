8d440fa7b72207f8f08186e6e3110c664b268262
UzairAjml <uzairajmal47@gmail.com>
2022-01-12 22:31:23 +0500

    updated

--- models/orderModel.js
+++ models/orderModel.js
@@ -28,9 +28,9 @@ const orderSchema = mongoose.Schema(
     ],
     shippingAddress: {
       address: { type: String, required: true },
-      city: { type: String, required: true },
-      postalCode: { type: String, required: true },
-      country: { type: String, required: true },
+      city: { type: String},
+      postalCode: { type: String},
+      country: { type: String },
     },
     paymentMethod: {
       type: String,
